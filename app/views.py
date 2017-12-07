52187e9d0132a6ce3b3f6a3522a0f7ad2d8740c6
Joe Krause <jkrause@e1z1r6p15.42.us.org>
2017-12-07 13:27:02 -0800

    Fix the ogg->wav conversion because of an oversight in file handling.

--- app/views.py
+++ app/views.py
@@ -44,9 +44,15 @@ def kift():
         else:
             mime_type = request.headers.get("Content-Type")
             if (mime_type == "audio/ogg"):
+                try:
+                    os.remove(uploads + "audio.ogg")
+                    os.remove(uploads + "audio.wav")
+                except:
+                    pass
                 ogg_file = open(uploads + "audio.ogg", "wb")
                 ogg_file.write(request.data)
-                #convert_ogg_to_wav(uploads + "audio.ogg")
+                ogg_file.close()
+                convert_ogg_to_wav(uploads + "audio.ogg")
                 return redirect("/kift")
         return redirect("/kift")
     return render_template("kift.html")
